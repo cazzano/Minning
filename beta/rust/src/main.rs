c84a15f5edb4f4c38dd45db4782636ab64bc7886
cazzano <cazzano.agent.47@gmail.com>
2025-03-22 17:02:34 +0500

    Importnat

--- beta/rust/src/main.rs
+++ beta/rust/src/main.rs
@@ -2,6 +2,8 @@ use std::env;
 
 // Import the initialize function from init.rs
 mod init;
+// Import the run module
+mod run;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -18,15 +20,29 @@ fn main() {
                     Err(e) => eprintln!("Error during initialization: {}", e),
                 }
             },
+
+            "run" => {
+                println!("Running XMR...");
+                
+                match run::run_xmr() {
+                    Ok(_) => println!("XMR executed successfully."),
+                    Err(e) => {
+                        eprintln!("Error running XMR: {}", e);
+                        std::process::exit(1);
+                    }
+                }
+            },
             _ => {
                 println!("Unknown command: {}", command);
                 println!("Available commands:");
                 println!("  ./main init - Initialize XMR");
+                println!("  ./main run - Run XMR");
             }
         }
     } else {
         println!("Hello, world!");
         println!("Available commands:");
         println!("  ./main init - Initialize XMR");
+        println!("  ./main run - Run XMR");
     }
 }
